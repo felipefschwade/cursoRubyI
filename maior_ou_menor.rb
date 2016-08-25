1cdf53e302f33f86f140b048d696fbad0fc034f4
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-08-24 20:46:58 -0400

    Corrigindo

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -11,7 +11,7 @@ puts "Escolhendo um número secreto entre 0 e 200... \n\n"
 # Definindo um número
 numeroSecreto = 175
 puts "Tentativa 1\n"
-puts "Digite o seu chutez \n"
+puts "Digite o seu chute \n"
 # Pega chute da entrada do usuário
 chute = gets
 puts "Será que acertou? Você chutou " + chute +"\n\n"
