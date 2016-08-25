1bf417feb7877ce4e17ba59aa3a8d78f3fccf26b
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-08-24 20:38:57 -0400

    Convertendo a entrada do usuário e comparando o chute com o número secreto

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -9,12 +9,18 @@ nome = gets
 puts 
 puts
 puts
+# Mensagem de comeco do jogo
 puts "Começaremos o jogo para voce " + nome
 puts 
 puts
+# Mensagem de escolha do numero
 puts "Escolhendo um número secreto entre 0 e 200..."
+# Definindo um número
 numeroSecreto = 175
 puts "Tentativa 1"
 puts "Digite o seu chute"
-gets chute
+# Pega chute da entrada do usuário
+chute = gets
 puts "Será que acertou? Você chutou " + chute
+# Compara o chute à entrada do usuário
+puts chute.to_i == numeroSecreto
