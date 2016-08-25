f469f3ff9613402f6da966b05941a0b98a4b1587
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-08-24 20:29:09 -0400

    Pedindo um chute para o usuário

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,10 +1,20 @@
+# Puts Imprime algo na tela
 puts "Bem-vindo ao jogo da adivinhação"
 puts 
 puts
 puts
 puts "Qual o seu nome?"
+#Gets captura a entrada de um usuário
 nome = gets
 puts 
 puts
 puts
 puts "Começaremos o jogo para voce " + nome
+puts 
+puts
+puts "Escolhendo um número secreto entre 0 e 200..."
+numeroSecreto = 175
+puts "Tentativa 1"
+puts "Digite o seu chute"
+gets chute
+puts "Será que acertou? Você chutou " + chute
