db8fce5490d23a762f09cc8422c532cde6c20204
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-08-24 20:46:25 -0400

    Refatorando

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,26 +1,19 @@
 # Puts Imprime algo na tela
-puts "Bem-vindo ao jogo da adivinhação"
-puts 
-puts
-puts
+puts "Bem-vindo ao jogo da adivinhação \n\n\n"
 puts "Qual o seu nome?"
 #Gets captura a entrada de um usuário
 nome = gets
-puts 
-puts
-puts
+puts "\n\n\n\n"
 # Mensagem de comeco do jogo
-puts "Começaremos o jogo para voce " + nome
-puts 
-puts
+puts "Começaremos o jogo para voce " + nome + "\n\n"
 # Mensagem de escolha do numero
-puts "Escolhendo um número secreto entre 0 e 200..."
+puts "Escolhendo um número secreto entre 0 e 200... \n\n"
 # Definindo um número
 numeroSecreto = 175
-puts "Tentativa 1"
-puts "Digite o seu chute"
+puts "Tentativa 1\n"
+puts "Digite o seu chutez \n"
 # Pega chute da entrada do usuário
 chute = gets
-puts "Será que acertou? Você chutou " + chute
+puts "Será que acertou? Você chutou " + chute +"\n\n"
 # Compara o chute à entrada do usuário
 puts chute.to_i == numeroSecreto
