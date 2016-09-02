965b466c5973237dcbe1ec7676818ab3ed9b67f2
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-09-01 21:06:18 -0400

    Aula 3 - Atividade 1 - Extraindo o método deBoasVindas

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,13 +1,18 @@
-# Puts Imprime algo na tela
-puts "Bem-vindo ao jogo da adivinhação \n\n\n"
-puts "Qual o seu nome?"
-#Gets captura a entrada de um usuário
-nome = gets
-puts "\n\n\n\n"
-# Mensagem de comeco do jogo
-puts "Começaremos o jogo para voce " + nome + "\n\n"
-# Mensagem de escolha do numero
-puts "Escolhendo um número secreto entre 0 e 200... \n\n"
+def deBoasVindas
+	# Puts Imprime algo na tela
+	puts "Bem-vindo ao jogo da adivinhação \n\n\n"
+	puts "Qual o seu nome?"
+	#Gets captura a entrada de um usuário
+	nome = gets
+	puts "\n\n\n\n"
+	# Mensagem de comeco do jogo
+	puts "Começaremos o jogo para voce " + nome + "\n\n"
+	# Mensagem de escolha do numero
+	puts "Escolhendo um número secreto entre 0 e 200... \n\n"
+end
+
+
+deBoasVindas
 limiteDeTentivas = 3
 # Definindo um número
 numeroSecreto = 175
